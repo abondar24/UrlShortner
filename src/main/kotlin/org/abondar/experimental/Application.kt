4b43ceea7db0d4dcc1e5c9a13686ff2cb4235cbb
Alex Bondar <abondar1992@gmail.com>
2024-07-14 08:34:48 +0200

    Initial commit

---
+++ src/main/kotlin/org/abondar/experimental/Application.kt
@@ -0,0 +1,15 @@
+package org.abondar.experimental
+
+import io.ktor.server.application.*
+import org.abondar.experimental.plugins.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureHTTP()
+    configureMonitoring()
+    configureSerialization()
+    configureRouting()
+}
